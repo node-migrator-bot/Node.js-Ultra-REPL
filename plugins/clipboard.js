3ad7794b8306531dfecc5accb0338492ba4a18db
Brandon Benvie <brandon@bbenvie.com>
2012-04-02 10:15:38 -0400

    Fixes for people to install it betterness.

--- plugins/clipboard.js
+++ plugins/clipboard.js
@@ -1,7 +1,7 @@
 // windows bindings for now, need to add others to clipboard module
 if (process.platform !== 'win32') return module.exports = [];
 
-var clipboard = require('clipboard');
+try { var clipboard = require('clipboard'); } catch (e) { return module.exports = []; }
 var Results = require('../lib/Results').Success
 
 
@@ -17,51 +17,50 @@ module.exports = {
       return output;
     });
   },
-  commands: [
-    { name: 'Copy',
-      help: 'Copy current line or current results to clipboard',
-      defaultTrigger: api.keybind('ctrl+c'),
-      action: function(){
-        if (this.rli.line.trim().length) {
+  commands: [{
+    name: 'Copy',
+    help: 'Copy current line or current results to clipboard',
+    defaultTrigger: api.keybind('ctrl+c'),
+    action: function(){
+      if (this.rli.line.trim().length) {
 
-          clipboard.write(this.rli.line);
-          var message = 'Copied line to clipboard';
+        clipboard.write(this.rli.line);
+        var message = 'Copied line to clipboard';
 
-        } else if (this.context.lastResult) {
+      } else if (this.context.lastResult) {
 
-          clipboard.write(this.context.lastResult);
-          var message = 'Copied last result to clipboard';
+        clipboard.write(this.context.lastResult);
+        var message = 'Copied last result to clipboard';
 
-        }
-        message && this.rli.timedWrite('topright', message, 'bgbblack');
       }
-    },
-    { name: 'Paste',
-      help: 'Paste the clipboard text or JS Object. (You can paste objects between contexts)',
-      defaultTrigger: api.keybind('ctrl+v'),
-      action: function(){
-        var items = clipboard.readAll();
-        if ('jsobject' in items) {
+      message && this.rli.timedWrite('topright', message, 'bgbblack');
+    }
+  },
+  { name: 'Paste',
+    help: 'Paste the clipboard text or JS Object. (You can paste objects between contexts)',
+    defaultTrigger: api.keybind('ctrl+v'),
+    action: function(){
+      var items = clipboard.readAll();
+      if ('jsobject' in items) {
 
-          var obj = items.jsobject.value;
-          if (obj.isResult) {
-            obj = obj.completion || obj.globals || obj.error;
-          }
-          this.context.current.locals.clipboard = obj;
-          return new Results(this.context.current, null, obj, null, 'Pasted object to local variables as "clipboard"');
-
-        } else if ('json' in items) {
-
-          return JSON.parse(items.json.value);
-
-        } else if ('ascii' in items) {
-
-          var result = this.context.current.run(items.ascii.value);
-          result.label = 'Executed code from clipboard';
-          this.writer(result);
-          //return result;
+        var obj = items.jsobject.value;
+        if (obj.isResult) {
+          obj = obj.completion || obj.globals || obj.error;
         }
+        this.context.current.locals.clipboard = obj;
+        return new Results(this.context.current, null, obj, null, 'Pasted object to local variables as "clipboard"');
+
+      } else if ('json' in items) {
+
+        return JSON.parse(items.json.value);
+
+      } else if ('ascii' in items) {
+
+        var result = this.context.current.run(items.ascii.value);
+        result.label = 'Executed code from clipboard';
+        this.writer(result);
+        //return result;
       }
     }
-  ]
+  }]
 }
